bd83ca8f558ec377ea5f8a091a7a915b067cb7d9
Sean Rodriguez <sean.rodriguez@outlook.com>
2024-07-30 22:04:24 -0400

    Add dark mode

--- Joover.Web/src/app/app.component.ts
+++ Joover.Web/src/app/app.component.ts
@@ -1,17 +1,26 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgClass],
   template: `
-    <h1>Welcome to {{ title }}!</h1>
-
-    <router-outlet />
+    <div [ngClass]="{ dark: darkMode() }" class="h-screen w-screen p-4 bg-gray-200 dark:bg-zinc-900 dark:text-white">
+      <div class="flex justify-end">
+        <button
+          class="rounded border border-zinc-500 px-2 py-1 hover:bg-gray-300 active:bg-gray-400 dark:hover:bg-zinc-800 dark:active:bg-zinc-700"
+          (click)="darkMode.set(!darkMode())"
+        >
+          Switch to {{ darkMode() ? 'Light' : 'Dark' }} Mode
+        </button>
+      </div>
+      <router-outlet />
+    </div>
   `,
   styles: [],
 })
 export class AppComponent {
-  title = 'Joover.Web';
+  protected darkMode = signal<boolean>(true);
 }
