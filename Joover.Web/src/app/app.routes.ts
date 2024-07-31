f420314f12f9f2ff00ff7c18ff8a85d88318dfb5
Sean Rodriguez <sean.rodriguez@outlook.com>
2024-07-30 21:23:25 -0400

    Add navigation to Dashboard component

--- Joover.Web/src/app/app.routes.ts
+++ Joover.Web/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () => import('../app/dashboard/dashboard.component').then((m) => m.DashboardComponent),
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+  },
+];
